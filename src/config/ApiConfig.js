7ea2c9ca5c0902266285c32c62a93efae6b16838
nauht1 <thuanngo454@gmail.com>
2024-07-24 13:43:06 +0700

    update gitignore, api path

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const baseURL = " https://chimp-direct-truly.ngrok-free.app/api/v1";
+// const baseURL = " https://chimp-direct-truly.ngrok-free.app/api/v1";
 // const baseURL = "http://localhost:8080/api/v1";
+const baseURL = "http://ec2-3-26-21-231.ap-southeast-2.compute.amazonaws.com/api/v1";
 
 const axiosToken = axios.create({
   baseURL: baseURL,
